41653a6d9c9884b70fe15596e00d587472393f79
Atremis <flamingphoneix@googlemail.com>
2024-09-17 12:02:27 +0100

    Fix: In my_calc muliply infinite loop

--- my_calc.py
+++ my_calc.py
@@ -5,3 +5,7 @@ def add_numbers(num1, num2):
 def subtract_numbers(num1, num2):
     result = num1 - num2
     return result
+
+def multiply_numbers(num_1, num_2):
+    result = num_1 * num_2
+    return result
