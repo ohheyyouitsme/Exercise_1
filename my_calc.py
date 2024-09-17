b6f62295976fd680cee19e0e9fb676cc3ecfc01a
Atremis <flamingphoneix@googlemail.com>
2024-09-17 14:55:19 +0100

    my_calc: added divide function

--- my_calc.py
+++ my_calc.py
@@ -9,3 +9,7 @@ def subtract_numbers(num1, num2):
 def multiply_numbers(num_1, num_2):
     result = num_1 * num_2
     return result
+
+def divide_numbers(num_1, num_2):
+    result = num_1 / num_2
+    return result
